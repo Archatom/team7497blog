3b1bc65d1b9fb339beb28ac5180a57964c6fb017
Archatom <78838261+Archatom@users.noreply.github.com>
2022-07-21 13:04:11 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '33a72c3c631c4e60a40f8d314d8e190b',
+  rootNotionPageId: '5fcb140aaad14470b9e9460804d112c1',
 
   // if you want to restrict pages to a single notion workspace (optional)
-  // (this should be a Notion ID; see the docs for how to extract this)
+  // (this should be a Notion qID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
